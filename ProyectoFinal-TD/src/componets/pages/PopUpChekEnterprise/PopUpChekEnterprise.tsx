83c2489e594e1b6455946fc5bba50dae5e181dcd
EmilioBaigorria <emiliobaigorriafacultad@gmail.com>
2024-10-29 09:31:22 -0300

    Boton cerrar en ver empresa ahora es mas bonito

--- ProyectoFinal-TD/src/componets/pages/PopUpChekEnterprise/PopUpChekEnterprise.tsx
+++ ProyectoFinal-TD/src/componets/pages/PopUpChekEnterprise/PopUpChekEnterprise.tsx
@@ -31,7 +31,7 @@ return (
             <h1 className={styles.text_font_format}>Razon social: {elementActive?.razonSocial}</h1>
             <h1 className={styles.text_font_format}>Cuit: {elementActive?.cuit}</h1>
             <h1 className={styles.text_font_format}>Logo:
-                <img src={elementActive?.logo} style={{width:"10rem"}} /></h1>
+                <img src={elementActive?.logo} style={{width:"25%"}} /></h1>
             <div className={styles.button_container}>
                 <Button className={styles.button_Style} variant="danger"  onClick={()=>{
                     handleCloseModal()
