27f7489c4faf507f201b3ea1efd4883e311ab42c
Brandon Munoz <byael.munos@gmail.com>
2024-10-30 21:34:46 -0300

    corrijo error null en elementActive.logo

--- ProyectoFinal-TD/src/componets/pages/PopUpChekEnterprise/PopUpChekEnterprise.tsx
+++ ProyectoFinal-TD/src/componets/pages/PopUpChekEnterprise/PopUpChekEnterprise.tsx
@@ -35,7 +35,9 @@ export const PopUpChekEnterprise: FC<IDisplayPopUp> = ({ displayModalCheckEnterp
                 <h1 className={styles.text_font_format}>Razon social: {elementActive?.razonSocial}</h1>
                 <h1 className={styles.text_font_format}>Cuit: {elementActive?.cuit}</h1>
                 <h1 className={styles.text_font_format}>Logo:
-                    <img src={elementActive?.logo} style={{ width: "25%" }} /></h1>
+                    <img src={elementActive?.logo ?? ''} style={{ width: "25%" }} />
+                </h1>
+                    
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleCloseModal}>
