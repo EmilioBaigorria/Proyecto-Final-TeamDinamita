542187f97ced37b77206104376b078f738ea4973
Emilio desde la pc chiquita <emiliobaigorriafacultad@gmail.com>
2024-11-07 09:44:17 -0300

    Nuevo sistema de gaurdado mas eficiente implementado, solo falta añadir la capacidad de guardado, elementos basicos de añadir sucursal

--- ProyectoFinal-TD/src/componets/pages/PopUpCreateOffice/PopUpCreateOffice.tsx
+++ ProyectoFinal-TD/src/componets/pages/PopUpCreateOffice/PopUpCreateOffice.tsx
@@ -1,60 +1,7 @@
-import {  ChangeEvent, FC, useEffect, useState } from 'react'
-import { Button, FloatingLabel, Form, Modal } from 'react-bootstrap'
-import { useAppSelector } from '../../../hooks/redux'
-import { ISucursal } from '../../../types/dtos/sucursal/ISucursal'
-import { UploadImage } from '../../UploadImage'
-import styles from "./PopUpEditOffice.module.css"
-import { IUpdateSucursal } from '../../../types/dtos/sucursal/IUpdateSucursal'
-const API_URL = import.meta.env.VITE_API_URL;
+import React from 'react'
 
-interface IPopUpEditOffice{
-    displayPopUpEditOffice :boolean,
-    setDisplayPopUpEditOffice: Function
-}
-
-export const PopUpEditOffice: FC<IPopUpEditOffice> = ({displayPopUpEditOffice,setDisplayPopUpEditOffice}) => {
-    const activeOffice : ISucursal | null = useAppSelector(
-        (state)=>state.ActiveOfficeReducer.activeOffice
-    )
-    
-    const initialValues ={
-        idEmpresa: activeOffice?.empresa.id || 0,
-        nombre: activeOffice?.nombre|| "" ,
-        id: activeOffice?.id|| 0,
-        eliminado: activeOffice?.eliminado || false,
-        latitud:activeOffice?.latitud|| 0,
-        longitud: activeOffice?.longitud || 0,
-        calle: activeOffice?.calle|| "",
-        numero: activeOffice?.domicilio.numero || 0,
-        cp: activeOffice?.domicilio.cp || 0,
-        piso:activeOffice?.domicilio.piso || 0,
-        nroDpto:activeOffice?.domicilio.nroDpto || 0,
-        idLocalidad:activeOffice?.domicilio.localidad.id ||0,
-        logo:activeOffice?.logo||"",
-        categorias:activeOffice?.categorias|| [],
-        esCasaMatriz:activeOffice?.esCasaMatriz || false,
-        horarioApertura: activeOffice?.horarioApertura|| "",
-        horarioCierre: activeOffice?.horarioCierre|| ""
-    }   
-    const[updatedSucursal,setUpdatedsucursal]=useState(initialValues)
-    const[logo,setLogo]=useState<string | null>(null)
-    
-    const handleClose = () => setDisplayPopUpEditOffice(false);
-    const handleSave=()=>{
-        console.log(updatedSucursal)
-    }
-    
-    const handleShow = () => setDisplayPopUpEditOffice(true);
-    
-
-    const handleChangeInputs = (event: ChangeEvent<HTMLInputElement>)=>{
-        const {value, name} = event.target
-        
-        setUpdatedsucursal((prev)=>({...prev, [`${name}`]: value}))
-    }
-    
-
-return (
+export const PopUpCreateOffice = () => {
+  return (
     <>
     
     <Modal show={displayPopUpEditOffice} onHide={handleClose} fullscreen={true}>
@@ -85,16 +32,36 @@ return (
                     }} value={updatedSucursal.horarioCierre}  type="text" name='horarioCierre' onChange={handleChangeInputs} />
                 </FloatingLabel>
                 <div>
-                    <UploadImage image={logo} setImage={setLogo} />
+                    <UploadImage image={updatedSucursal.logo} setImage={()=>{
+                        updatedSucursal.logo=logo
+                    }} />
                 </div>
             </div>
             {/*Segundo set de inputs */}
             <div className={styles.inpunts_style}>
                 <FloatingLabel
-                    label="IdLocalidad">
+                    label="Pais">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.idLocalidad}  type="text" name='idLocalidad' onChange={handleChangeInputs} />
+                    }} value={country}  type="text" placeholder="Argentina" onChange={(e)=>{
+                        (e.target.value)
+                    }} />
+                </FloatingLabel>
+                <FloatingLabel
+                    label="Provincia">
+                    <Form.Control style={{
+                            width:"20rem",  
+                    }} value={province}  type="text" placeholder="Mendoza" onChange={(e)=>{
+                        setProvince(e.target.value)
+                    }} />
+                </FloatingLabel>
+                <FloatingLabel
+                    label="Localidad">
+                    <Form.Control style={{
+                            width:"20rem",  
+                    }} value={locality}  type="text" placeholder="Mendoza" onChange={(e)=>{
+                        setLocality(e.target.value)
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     label="Latitud">
@@ -115,31 +82,41 @@ return (
                     label="Nombre Calle">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.calle}  type="text" name='calle' onChange={handleChangeInputs} />
+                    }} value={updatedSucursal.domicilio.calle}  type="text" name='domicilio' onChange={(e)=>{
+                        updatedSucursal.domicilio.calle=e.target.value
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     label="Numero de Calle">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.numero}  type="text" name='numero' onChange={handleChangeInputs} />
+                    }} value={streetNumber}  type="text" placeholder="1212" onChange={(e)=>{
+                        updatedSucursal.domicilio.numero=Number(e.target.value)
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     label="Codigo postal">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.cp}  type="text" name='cp' onChange={handleChangeInputs}/>
+                    }} value={postalCode}  type="text" placeholder="4450" onChange={(e)=>{
+                        setPostalCode(Number(e.target.value))
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     label="Piso">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.piso}  type="text" name='piso' onChange={handleChangeInputs} />
+                    }} value={floorNumber}  type="text" placeholder="3" onChange={(e)=>{
+                        setFloorNumber(Number(e.target.value))
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     label="Departamento">
                     <Form.Control style={{
                             width:"20rem",  
-                    }} value={updatedSucursal.nroDpto}  type="text" name='nroDpto' onChange={handleChangeInputs} />
+                    }} value={deparmentNumber}  type="text" placeholder="14" onChange={(e)=>{
+                        setDeparmentNumber(Number(e.target.value))
+                    }} />
                 </FloatingLabel>
             </div>
         </div>
@@ -156,5 +133,5 @@ return (
         </Modal.Footer>
     </Modal>
     </>
-)
+  )
 }
