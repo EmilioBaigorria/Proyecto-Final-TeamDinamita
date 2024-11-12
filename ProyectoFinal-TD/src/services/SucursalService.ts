1e5d5a8747d46149eb81129e2562e2e937c6e258
Emilio desde la pc chiquita <emiliobaigorriafacultad@gmail.com>
2024-11-12 09:30:07 -0300

    Crear sucursal ya funciona

--- ProyectoFinal-TD/src/services/SucursalService.ts
+++ ProyectoFinal-TD/src/services/SucursalService.ts
@@ -6,18 +6,20 @@ import { BackendClient } from "./BackendClient";
 export class SucursalService extends BackendClient<ISucursal|IUpdateSucursal | ICreateSucursal> {
     async updateSucursal(id: number, data: IUpdateSucursal): Promise<ISucursal|IUpdateSucursal | ICreateSucursal> {
       this.baseUrl=this.baseUrl+"/sucursales/update"
-        return await this.put(id, data);
+      return await this.put(id, data)
       }
-      async sucursalPorEmpresa(id: number): Promise<ISucursal|IUpdateSucursal | ICreateSucursal | null> {
-        this.baseUrl=this.baseUrl+"/sucursales/porEmpresa"
-        const rep=await this.getById(id)
-        if(rep){
-          return rep
-        }else{
-          return null
-        }
-        
-        }
-      
+    async createSucursal(data :ICreateSucursal):Promise<ISucursal|IUpdateSucursal | ICreateSucursal>{
+      this.baseUrl=this.baseUrl+"/sucursales/create"
+      return await this.post(data)
+    }
+    async sucursalPorEmpresa(id: number): Promise<ISucursal|IUpdateSucursal | ICreateSucursal | null> {
+      this.baseUrl=this.baseUrl+"/sucursales/porEmpresa"
+      const rep=await this.getById(id)
+      if(rep){
+        return rep
+      }else{
+        return null
+      }}
+    
     
 }
