b5934440b219c1c18079b593beec9576126a498f
Emilio desde la pc chiquita <emiliobaigorriafacultad@gmail.com>
2024-11-14 09:19:35 -0300

    Nuevo metodo en categoria services, update categoria por Id

--- ProyectoFinal-TD/src/services/CategoriaService.ts
+++ ProyectoFinal-TD/src/services/CategoriaService.ts
@@ -3,4 +3,16 @@ import { ICreateCategoria } from "../types/dtos/categorias/ICreateCategoria";
 import { IUpdateCategoria } from "../types/dtos/categorias/IUpdateCategoria";
 import { BackendClient } from "./BackendClient";
 
-export class CategoriaService extends BackendClient<ICreateCategoria | ICategorias | IUpdateCategoria>{}
+export class CategoriaService extends BackendClient<ICreateCategoria | ICategorias | IUpdateCategoria>{
+    async updateCategoria(id:number, data:IUpdateCategoria):Promise<IUpdateCategoria>{
+        const response=await fetch(`${this.baseUrl}/update/${id}`,{
+            method: "PUT",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        const newData=await response.json()
+        return newData as IUpdateCategoria
+    }
+}
