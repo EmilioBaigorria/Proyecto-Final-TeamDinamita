02273d6c7ed98295d6d5d36021c0dd186bfb0dd2
Tonga <gaston.astudillo@gmail.com>
2024-11-18 22:37:04 -0300

    cambios esteticos categoria

--- ProyectoFinal-TD/src/componets/ui/CategoriasPage/CategoriasPage.tsx
+++ ProyectoFinal-TD/src/componets/ui/CategoriasPage/CategoriasPage.tsx
@@ -7,38 +7,37 @@ import { CategoryDropdown } from "../CategoryDropdown/CategoryDropdown";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-interface ICategoriasPage{
-  office:ISucursal
+interface ICategoriasPage {
+  office: ISucursal
 }
 
-export const CategoriasPage:FC<ICategoriasPage> = ({office}) => {
-  
-  const cateService=new CategoriaService(API_URL)
-  const [categories,setCategories]=useState<ICategorias[] | null>([])
+export const CategoriasPage: FC<ICategoriasPage> = ({ office }) => {
 
-  const getCategories=useEffect(()=>{
-    const getCate=async ()=>{
+  const cateService = new CategoriaService(API_URL)
+  const [categories, setCategories] = useState<ICategorias[] | null>([])
+
+  const getCategories = useEffect(() => {
+    const getCate = async () => {
       try {
-        const categoryData=await cateService.todasCategoriasPadresPorSucursal(office?.id)
+        const categoryData = await cateService.todasCategoriasPadresPorSucursal(office?.id)
         setCategories(categoryData)
       } catch (error) {
-        console.log("Hubo un error con el fetch de las categorias: ",error)
+        console.log("Hubo un error con el fetch de las categorias: ", error)
       }
     }
     getCate()
-    
-  },[])
+
+  }, [])
   return (
     <div>
-      <h2>{office ? office.nombre: "hubo un error"}</h2>
+      <h2>{office ? office.nombre : "Hubo un error."}</h2>
       <div style={{
-        width:"90%",
-        display:"flex",
-        flexDirection:"column",
-        backgroundColor:"red"
+        width: "90%",
+        display: "flex",
+        flexDirection: "column"
       }}>
-        {categories?.map((cate)=>(
-          <CategoryDropdown category={cate}/>
+        {categories?.map((cate) => (
+          <CategoryDropdown category={cate} />
         ))
         }
       </div>
