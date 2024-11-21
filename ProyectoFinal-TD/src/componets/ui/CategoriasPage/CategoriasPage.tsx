12520e61a46c1658d55023c66c981003e7aaa8a6
Tonga <gaston.astudillo@gmail.com>
2024-11-21 01:08:43 -0300

    infinite requests solucionado

--- ProyectoFinal-TD/src/componets/ui/CategoriasPage/CategoriasPage.tsx
+++ ProyectoFinal-TD/src/componets/ui/CategoriasPage/CategoriasPage.tsx
@@ -6,8 +6,8 @@ import { CategoryDropdown } from "../CategoryDropdown/CategoryDropdown";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-interface ICategoriasPage{
-  office:ISucursal |null
+interface ICategoriasPage {
+  office: ISucursal | null
 }
 
 export const CategoriasPage: FC<ICategoriasPage> = ({ office }) => {
@@ -15,10 +15,10 @@ export const CategoriasPage: FC<ICategoriasPage> = ({ office }) => {
   const cateService = new CategoriaService(API_URL)
   const [categories, setCategories] = useState<ICategorias[] | null>([])
 
-  const getCategories = useEffect(() => {
+  useEffect(() => {
     const getCate = async () => {
       try {
-        const categoryData=await cateService.todasCategoriasPadresPorSucursal(Number(office?.id))
+        const categoryData = await cateService.todasCategoriasPadresPorSucursal(Number(office?.id))
         setCategories(categoryData)
       } catch (error) {
         console.log("Hubo un error con el fetch de las categorias: ", error)
@@ -26,18 +26,18 @@ export const CategoriasPage: FC<ICategoriasPage> = ({ office }) => {
     }
     getCate()
 
-  }, [cateService, office?.id])
+  }, [office?.id])
   return (
-    <div style={{overflow: "auto"}}>
+    <div style={{ overflow: "auto" }}>
       <h2>{office ? office.nombre : "Hubo un error."}</h2>
       <div style={{
-        width:"90%",
-        display:"flex",
-        flexDirection:"column",
-        
+        width: "90%",
+        display: "flex",
+        flexDirection: "column",
+
       }}>
         {categories?.map((cate) => (
-          <CategoryDropdown key={cate.id} category={cate} idSucursal={office!.id}/>
+          <CategoryDropdown key={cate.id} category={cate} idSucursal={office!.id} />
         ))
         }
       </div>
